de9859db2ae630dd9d81d2d45614143f5431ac0d
Dialtor <dialtordev@gmail.com>
2022-09-30 20:57:04 -0500

    [Init] Configurando Entorno

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>TheCocktailzuela papa</h1>
-    </div>
+    <>
+    <Header/>
+    </>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles/Header.module.css';
+
+const Header = () => {
+	return <div className={styles.header}>Header</div >;
+};
+
+export default Header;
