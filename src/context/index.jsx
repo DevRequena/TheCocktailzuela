8b0220e10f130f0f55359a766bd3b2b614d1a7bb
DevRequena <devcesarperez@gmail.com>
2022-10-01 12:49:56 -0400

    [update] trabajando sobre la API

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -9,6 +9,21 @@ export const DataProvider = ({ children }) => {
 
   //Hooks API get return
 
+
+
+
+  //  Ingredientes (ron, vodka, gin)
+  // 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin'
+
+  // buscar cocktails
+  // www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+
+  // Add /preview to the end of the cocktail image URL
+  // /images/media/drink/vrwquq1478252802.jpg/preview
+
+  // random cocktail
+  // www.thecocktaildb.com/api/json/v1/1/random.php
+
   return (
     <DataContext.Provider value={{
 
